c726bff89bdd209608f742eb40bf4be783ed1461
Harmaaw <muuta.mielesi@gmail.com>
2026-06-23 13:14:36 -0700

    Part 2 completed

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,23 +3,35 @@ from app import create_app
 from unittest import mock
 import datetime
 
+
 def mocked_requests_get(*args, **kwargs):
     class MockResponse:
         def __init__(self, status_code, json_data):
             self.status_code = status_code
             self.json_data = json_data
-            
+
         def json(self):
             return self.json_data
 
     if args[0] == "https://tle.ivanstanojevic.me/api/tle/25544":
-        tmp_response = {"@context":"https:\\/\\/www.w3.org\\/ns\\/hydra\\/context.jsonld","@id":"https:\\/\\/tle.ivanstanojevic.me\\/api\\/tle\\/25544","@type":"Tle","satelliteId":25544,"name":"ISS (ZARYA)","date":"2026-06-07T21:37:51+00:00","line1":"1 25544U 98067A   26158.90128687  .00007994  00000+0  14961-3 0  9995","line2":"2 25544  51.6338 346.0598 0006926 145.2709 214.8733 15.49660544570312"}
+        tmp_response = {
+            "@context": "https://www.w3.org/ns/hydra/context.jsonld",
+            "@id": "https://tle.ivanstanojevic.me/api/tle/25544",
+            "@type": "Tle",
+            "satelliteId": 25544,
+            "name": "ISS (ZARYA)",
+            "date": "2026-06-07T21:37:51+00:00",
+            "line1": "1 25544U 98067A   26158.90128687  .00007994  00000+0  14961-3 0  9995",
+            "line2": "2 25544  51.6338 346.0598 0006926 145.2709 214.8733 15.49660544570312"
+        }
         return MockResponse(200, tmp_response)
+
     elif args[0] == "https://tle.ivanstanojevic.me/api/tle/0":
-        tmp_response = {"response":{"message":"Unable to find record with id 0"}}
+        tmp_response = {"response": {"message": "Unable to find record with id 0"}}
         return MockResponse(404, tmp_response)
 
     return MockResponse(404, None)
+
 
 class MainTestCase(unittest.TestCase):
     def setUp(self):
@@ -30,10 +42,19 @@ class MainTestCase(unittest.TestCase):
 
     def tearDown(self):
         self.app_context.pop()
-    
+
     @mock.patch('app.main.views.requests.get', side_effect=mocked_requests_get)
     def test_calculate(self, mock_get):
         response = self.client.post('/calculate', data={
+            'satellite_id': '25544',
             'input_date_time': datetime.datetime(2026, 6, 1, 12, 0)
         })
-        self.assertTrue("Position (X, Y, Z) in km: (888.7196363510753, -4143.88732612825, -5323.6785654496225)" in response.get_data(as_text=True))
+
+        text = response.get_data(as_text=True)
+
+        self.assertIn("Position (X, Y, Z) in km:", text)
+
+        self.assertIn(
+            "TLE DateTime: 2026-06-07T21:37:51+00:00",
+            text
+        )
